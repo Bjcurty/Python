0f40780566aa72aa6bdead930096a38402d78aa4
Bradley Curtis <bradley@haulynx.com>
2020-05-11 08:53:08 -0700

    Working through the_basics

--- the_basics/basics.py
+++ the_basics/basics.py
@@ -1,2 +1,16 @@
-import datetime
-print(datetime.datetime.now())
+# import datetime
+# print("The date and time is:", datetime.datetime.now())
+# # print(datetime.datetime.now())
+
+# monday_temperatures = [9.1, 8.8, 7.5]
+
+# # dictionary:
+# for grades in student_grades.item():
+#                             .keys():
+#                             .values():
+
+temps = [221, 234, 340, -9999, 230]
+
+new_temps = [temp / 10 for temp in temps if temp != -9999]
+
+print(new_temps)
